f18cfc1d2e33ad5f6f4fd6322b88dfa39152cacb
verszz <zikri.zulfa@ui.ac.id>
2023-09-22 11:30:21 +0700

    CS03 - Pengerjaan CS03

--- zikriZulfaAzhimJBusRS/JBus.java
+++ zikriZulfaAzhimJBusRS/JBus.java
@@ -71,12 +71,12 @@ public class JBus{
   }
   
     public static void main(String[] args) {
-    Bus testBus = createBus();
-    System.out.println(testBus.id);
-    System.out.println(testBus.name);
-    System.out.println(testBus.facility);
-    System.out.println(testBus.price.price);
-    System.out.println(testBus.capacity);
+    //Bus testBus = createBus();
+    //System.out.println(testBus.id);
+    //System.out.println(testBus.name);
+    //System.out.println(testBus.facility);
+    //System.out.println(testBus.price.price);
+    //System.out.println(testBus.capacity);
     
     Payment testPayment = new Payment(1, 1, 1, "A", 1, "A", "A");
     Invoice testInvoice = new Invoice(2,2,2, "B");
